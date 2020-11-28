e0f921b39fb305449bcf404b98de87bc2f845b50
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-11-28 20:37:07 +0300

    implemented show in main

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * @author Stanislav Stoianov
+ */
+
+#include "../include/automata.h"
+
+int main() {
+    auto *automata = new Automata();
+    automata->on();
+    Automata::getMenu();
+    automata->coin(20);
+    automata->cancel();
+    automata->coin(10);
+    automata->choice(5);
+    automata->coin(0);
+    automata->choice(6);
+    automata->off();
+    return 0;
+}
+
