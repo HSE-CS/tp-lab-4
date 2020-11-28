c0703682ddea4d939a96479db880f70bb9e532e6
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-11-28 20:36:19 +0300

    create automata template

---
+++ include/automata.h
@@ -0,0 +1,58 @@
+/*
+ * @author Stanislav Stoianov
+ */
+
+#ifndef TASK1_AUTOMATA_H
+#define TASK1_AUTOMATA_H
+
+#include <string>
+#include <vector>
+
+enum class STATUS {
+    OFF, WAIT, ACCEPT, CHECK, COOK
+};
+
+struct PRODUCT {
+    std::string name;
+    unsigned int price;
+};
+
+class Automata {
+private:
+
+    unsigned int cash;
+
+    unsigned int order;
+
+    STATUS status;
+
+    static std::vector<PRODUCT> menu;
+
+    bool check();
+
+    bool cook();
+
+    bool finish();
+
+    unsigned int getCash() const;
+
+    unsigned int getChoice() const;
+
+public:
+    Automata();
+
+    bool on();
+
+    bool off();
+
+    bool coin(unsigned int money);
+
+    bool choice(unsigned int orderNum);
+
+    bool cancel();
+
+    static void getMenu();
+
+};
+
+#endif
