1644d0bb085570fcc5717e4dec30c4427d3d0a61
Ilya Urtyokov <urtilya@gmail.com>
2020-11-29 12:24:39 +0300

    +tests

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,121 +1,158 @@
 #include "Automata.h"
 #include <cstring>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
 
-Automata::Automata(){
-cash=0;
-state=OFF;
-chos=-1;
-count=3;
-menu={"COFFE","TEA","CACAO"};
-prices={29.99,19.99,49.99};
+Automata::Automata ()
+{
+  cash = 0;
+  state = OFF;
+  chos = -1;
+  count = 3;
+  menu = new char *[3]
+  {
+  "COFFE", "TEA", "CACAO"};
+  prices = new double[3]
+  {
+  29.99, 19.99, 49.99};
 //cin>>count;
 //menu= new char*(count);
 //prices= new double(count);
 //for (size_t i=0;i<count){cin>>menu[i]; cin>>prices[i];}
 }
 
-int Automata::on(){
-if (state==OFF){
-state=WAIT;
-return 0;
-}
-return -1;
-}
-
-int Automata::off(){
-if (state==WAIT){
-state=OFF;
-return 0;
-}
-return -1;
+int
+Automata::on ()
+{
+  if (state == OFF)
+    {
+      state = WAIT;
+      return 0;
+    }
+  return -1;
 }
 
-
-
-
-int Automata::finish(){
-if (state==COOK){
-state=WAIT;
-int ret=cash;
-cash=0;
-chos=-1;
-return ret;
-}
-return -1;
-}
-
-STATES Automata::getState() const{
-return state;
-}
-
-
-char** Automata::getMenu() const{
-char** ret= new char*(count);
-char* a,b;
-for (size_t i=0; i<count;i++){
-strcpy(a,menu[i]);
-b=DoubleToStr(prices[i],2);
-strcat(a," ");
-strcat(a,b);
-ret[i]=a;
-}
-if (state!=OFF)
-return ret;
-else
-return -1;
+int
+Automata::off ()
+{
+  if (state == WAIT)
+    {
+      state = OFF;
+      return 0;
+    }
+  return -1;
 }
 
 
 
-boolean Automata::check() const{
-if (state==CHECK){
-if (cash>=prices[chos])
-return TRUE;
-else return FALSE;
-}
-return -1;
+
+int
+Automata::finish ()
+{
+  if (state == COOK)
+    {
+      state = WAIT;
+      int ret = cash;
+      cash = 0;
+      chos = -1;
+      return ret;
+    }
+  return -1;
 }
 
-int Automata::coin(double a){
-if ((state!=OFF) &&(state!=COOK){
-cash+=a;
-chos=-1;
-state=ACCEPT;
-return 0
-}
-return -1;
+STATES
+Automata::getState () const
+{
+  return state;
 }
 
 
-int Automata::choice(int x){
-if (state==ACCEPT){
-state=CHECK;
-chos=x;
-return 0;
-}
-return -1;
+int
+Automata::getMenu (char **menu) const
+{
+  if (state == OFF)
+    return -1;
+  else
+    {
+      char **ret = new char *[count];
+      char *a;
+      for (size_t i = 0; i < count; i++)
+	{
+	  strcpy (a, menu[i]);
+	  strcat (a, " ");
+	  sprintf (a, "%g", prices[i]);
+	  ret[i] = a;
+	}
+      menu = ret;
+    }
 }
 
-int Automata::cook(){
-if ((state==CHECK)&&check()){
-state=COOK;
-return 0;
-}
-return -1;
+
+
+bool
+Automata::check () const
+{
+  if (state == CHECK)
+    {
+      if (cash >= prices[chos])
+	return true;
+      else
+	return false;
+    }
+  return -1;
 }
 
-int Automata::cansel(){
-if ((state==ACCEPT)||(state==check)){
-int ret =cash;
-cash=0
-chos=-1;
-state=WAIT;
-return ret;
+int
+Automata::coin (double a)
+{
+  if ((state != OFF) && (state != COOK))
+    {
+      cash += a;
+      chos = -1;
+      state = ACCEPT;
+      return 0;
+    }
+  return -1;
 }
-return -1
+
+
+int
+Automata::choice (int x)
+{
+  if (state == ACCEPT)
+    {
+      state = CHECK;
+      chos = x;
+      return 0;
+    }
+  return -1;
+}
+
+int
+Automata::cook ()
+{
+  if ((state == CHECK) && check ())
+    {
+      state = COOK;
+      return 0;
+    }
+  return -1;
+}
+
+int
+Automata::cansel ()
+{
+  if ((state == ACCEPT) || (state == CHECK))
+    {
+      int ret = cash;
+      cash = 0;
+      chos = -1;
+      state = WAIT;
+      return ret;
+    }
+  return -1;
 }
 
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include "Automata.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    Automata automata;
+    cout << STATES(automata.getState()) << endl;
+    automata.on();
+    cout << STATES(automata.getState()) << endl;
+    char** menu;
+    //automata.getMenu(menu);
+    //for (size_t i=0; i<3; i++) cout<<menu[i]<<endl;
+    cout << STATES(automata.getState()) << endl;
+    automata.coin(50);
+    cout << STATES(automata.getState()) << endl;
+    automata.coin(25);
+    cout << STATES(automata.getState()) << endl;
+    automata.choice(0);
+    cout << STATES(automata.getState()) << endl;
+    automata.coin(100);
+    cout << STATES(automata.getState()) << endl;
+    automata.choice(2);
+    cout << STATES(automata.getState()) << endl;
+    automata.cook();
+    cout << STATES(automata.getState()) << endl;
+    cout << automata.finish() << endl;
+    cout << STATES(automata.getState()) << endl;
+    cout << STATES(automata.getState()) << endl;
+    automata.off();
+    cout << STATES(automata.getState()) << endl;
+    return 0;
+}
