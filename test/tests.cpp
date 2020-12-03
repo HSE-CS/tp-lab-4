76ca02a367c1185ec3508e56741a24e7008213c8
Vladislav-Kanev <vokanev@edu.hse.ru>
2020-12-03 13:13:28 +0300

    You didn't see that

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,43 +1,47 @@
+// Copyright 2020 test
 #include "gtest/gtest.h"
-//#include "../include/Automata.h"
 #include "Automata.h"
 
 TEST(AutomataTest, test1) {
     Automata test;
-    EXPECT_EQ(test.getState(), OFF);              // проверка выключен ли автомат по умолчанию
+    EXPECT_EQ(test.getState(), OFF);  // проверка выключен ли автомат по умолчанию
 }
 
 TEST(AutomataTest, test2) {
     Automata test;
     test.on();
-    EXPECT_EQ(test.getState(), WAIT);             // меняется ли статус на WAIT
+    EXPECT_EQ(test.getState(), WAIT);  // меняется ли статус на WAIT
 }
 
 TEST(AutomataTest, test3) {
     Automata test;
+    test.on();
     test.coin(10);
-    EXPECT_EQ(test.getState(), ACCEPT);             // меняется ли статус на ACCEPT
+    EXPECT_EQ(test.getState(), ACCEPT);  // меняется ли статус на ACCEPT
 }
 
 TEST(AutomataTest, test4) {
     Automata test;
+    test.on();
     test.coin(10);
     test.choice(1);
-    EXPECT_EQ(test.getState(), WAIT);             // меняется ли статус на ACCEPT, если мало денег
+    EXPECT_EQ(test.getState(), WAIT);  // меняется ли статус на ACCEPT, если мало денег
 }
 
 TEST(AutomataTest, test5) {
     Automata test;
+    test.on();
     test.coin(40);
     test.choice(1);
-    EXPECT_EQ(test.getState(), WAIT);             // меняется ли статус на ACCEPT, если достаточно денег
+    EXPECT_EQ(test.getState(), WAIT);  // меняется ли статус на ACCEPT, если достаточно денег
 }
 
 TEST(AutomataTest, test6) {
     Automata test;
+    test.on();
     test.coin(1);
     test.finish();
-    EXPECT_EQ(test.getState(), 1);              // меняется ли статус при окончании работы
+    EXPECT_EQ(test.getState(), 1);  // меняется ли статус при окончании работы
 }
 
 TEST(AutomataTest, test7) {
@@ -54,6 +58,7 @@ TEST(AutomataTest, test8) {
 
 TEST(AutomataTest, test9) {
     Automata test;
+    test.on();
     test.coin(10);
     test.coin(30);
     EXPECT_EQ(test.getCoin(), 40);             // суммируются ли деньги
@@ -61,6 +66,7 @@ TEST(AutomataTest, test9) {
 
 TEST(AutomataTest, test10) {
     Automata test;
-    std::string menu = "1 Americano 25\n2 Espresso 35\n3 Cappuccino 40\n4 Long Black 38\n5 Hot Chocolate 30";
+    test.on();
+    std::string menu = "1 Americano 25\n2 Espresso 35\n3 Cappuccino 40\n4 Long Black 38\n5 Hot Chocolate 30\n";
     EXPECT_EQ(test.getMenu(), menu);        // проверка меню
 }
